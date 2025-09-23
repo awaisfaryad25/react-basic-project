57e1913430c024835e570141ccb02055393e654f
M Awais Ch <142403036+awaisfaryad25@users.noreply.github.com>
2025-09-23 19:54:08 +0500

    remove comments

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -6,16 +6,9 @@ const About = ({id}) => {
     <div id={id}>
       About Section
       <div className="p-6 flex flex-col gap-4">
-      {/* Primary Input */}
       <Input placeholder="Primary Input" variant="primary" />
-
-      {/* Outline Input */}
       <Input placeholder="Outline Input" variant="outline" />
-
-      {/* Underline Input */}
       <Input placeholder="Underline Input" variant="underline" />
-
-      {/* Custom Extra Class */}
       <Input placeholder="Custom Input" className="bg-gray-100" />
     </div>
     </div>
