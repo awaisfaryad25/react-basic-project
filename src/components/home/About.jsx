266640346f4cd770da1f71f734467bc10b3593fe
M Awais Ch <142403036+awaisfaryad25@users.noreply.github.com>
2025-09-29 19:18:42 +0500

    refine and consize code

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -5,41 +5,37 @@ import Select from '../global/Select'
 import Checkbox from '../global/Checkbox'
 import RadioGroup from '../global/RadioGroup'
 
-const About = ({id}) => {
-  return (
-    <div id={id}>
-      About Section
-      <div className="p-6 flex flex-col gap-4">
+const selectOptions = [
+  { label: "Option 1", value: "1" },
+  { label: "Option 2", value: "2" },
+  { label: "Option 3", value: "3" },
+]
+
+const radioOptions = [
+  { label: "Option A", value: "A" },
+  { label: "Option B", value: "B" },
+  { label: "Option C", value: "C" },
+]
+
+const About = ({ id }) => (
+  <div id={id}>
+    About Section
+    <div className="p-6 flex flex-col gap-4">
       <Input placeholder="Primary Input" variant="primary" />
       <Input placeholder="Custom Input" className="bg-gray-100" />
-
-      <Select
-        variant="primary"
-        options={[
-          { label: "Option 1", value: "1" },
-          { label: "Option 2", value: "2" },
-          { label: "Option 3", value: "3" },
-        ]}
-      />
       
+      <Select variant="primary" options={selectOptions} />
       <Textarea placeholder="Primary Textarea..." variant="primary" />
-
       <Checkbox label="Accept Terms" onChange={() => {}} />
-
+      
       <RadioGroup
         name="example"
-        options={[
-          { label: "Option A", value: "A" },
-          { label: "Option B", value: "B" },
-          { label: "Option C", value: "C" },
-        ]}
+        options={radioOptions}
         value="A"
-        onChange={(val) => console.log(val)}
+        onChange={console.log}
       />
-
     </div>
-    </div>
-  )
-}
+  </div>
+)
 
 export default About
