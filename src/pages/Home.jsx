510cf77d813127b8cf6edf1438459f718a0a54a9
M Awais Ch <142403036+awaisfaryad25@users.noreply.github.com>
2026-02-05 21:17:35 +0500

    change section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,14 @@
-// import { useEffect } from "react";
-// import { useLocation } from "react-router-dom";
-// import HeroSection from "../components/hero/HeroSection";
-// import Portfolio from "../components/portfolio/Portfolio";
-// import Services from "../components/services/Services";
-// import Feedback from "../components/feedback/Feedback";
-// import ContactUs from "../components/contact/ContactUs";
+
+import HeroSection from "../components/home/HeroSection";
 
 import About from "../components/home/About";
 
 const Home = () => {
-  // const location = useLocation();
-
-  // useEffect(() => {
-  //   if (location.hash) {
-  //     const sectionId = location.hash.replace("#", "");
-  //     const element = document.getElementById(sectionId);
-  //     if (element) {
-  //       element.scrollIntoView({ behavior: "smooth" });
-  //     }
-  //   } else {
-  //     window.scrollTo({ top: 0, behavior: "smooth" });
-  //   }
-  // }, [location.hash]);
 
   return (
     <div>
       <div className="space-y-4 md:space-y-8 mb-4 md:mb-8">
-        {/* <HeroSection id="hero" /> */}
+        <HeroSection id="hero" />
         <About id="about" />
         {/* <Portfolio id="portfolio" /> */}
         {/* <Services id="services" /> */}
