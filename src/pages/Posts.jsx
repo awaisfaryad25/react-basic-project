95c1c4e0f36746f01d216d2203bfa757afed1e9b
M Awais Ch <142403036+awaisfaryad25@users.noreply.github.com>
2026-03-05 22:09:40 +0500

    basic form design

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Input from '../components/global/Input'
+import Button from '../components/global/Button'
 
 const Posts = () => {
   return (
-    <div>Posts</div>
+    <div className=''>
+      <form action="" className='w-full max-w-xl mx-auto mt-8 p-12 rounded-lg shadow bg-[#f2f4fa]'>
+        <h1 className='text-2xl font-bold'>Posts Page</h1>
+
+        <div className="w-full max-w-sm mt-4 mx-auto space-y-4">
+          <Input type="file" placeholder="Enter name" className='mb-4'/>
+          <Input type="text" placeholder="Enter name" required className='mb-8'/>
+
+          <Button variant="primary" label='Submit'/>
+        </div>
+
+      </form>
+    </div>
   )
 }
 
